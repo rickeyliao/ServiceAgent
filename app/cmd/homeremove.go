3ea331c6f66d0bf0493bc6ab4d9c4cb5fe357b39
rickeyliao <rickey.liao@tutanota.com>
2019-10-11 08:47:44 +0800

    add to repo

--- app/cmd/homeremove.go
+++ app/cmd/homeremove.go
@@ -24,13 +24,8 @@ var homeipnbsaddrremove string
 // removeCmd represents the remove command
 var homeipremoveCmd = &cobra.Command{
 	Use:   "remove",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "remove a home ip item",
+	Long: `remove a home ip item`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if !CheckProcessReady() {
 			return
