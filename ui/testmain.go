1f874a3c969416c35f3179375847059d30043f1a
rickeyliao <rickey.liao@tutanota.com>
2019-10-16 10:20:11 +0800

    add to repo

--- ui/testmain.go
+++ ui/testmain.go
@@ -1,29 +1,11 @@
 package main
 
-import (
-	"net/http"
-	"log"
-	"github.com/rickeyliao/ServiceAgent/ui/handle"
-	"fmt"
-)
+import "github.com/rickeyliao/ServiceAgent/ui/uiserver"
 
 func main()  {
-
-	mux:=http.NewServeMux()
-
-	mux.Handle("/css/", http.FileServer(http.Dir("staticfile")))
-	mux.Handle("/js/", http.FileServer(http.Dir("staticfile")))
-
-
-	mux.Handle("/login/",&handle.LoginHandle{})
-	mux.Handle("/ajax/",&handle.AjaxHandle{})
-	mux.Handle("/admin/",&handle.AdminHandle{})
-
-	mux.Handle("/",&handle.NotFound404{})
-
-	fmt.Println("Http Server Start at: 9527")
-
-	httpserver := &http.Server{Addr: ":9527", Handler: mux}
-
-	log.Fatal(httpserver.ListenAndServe())
+	uiserver.StartUIServer()
 }
+
+
+
+
