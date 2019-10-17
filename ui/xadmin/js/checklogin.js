cf8be4093d53f7d1c27cc83cbc0c0b5167cf3c35
rickeyliao <rickey.liao@tutanota.com>
2019-10-17 15:55:00 +0800

    add to repo

---
+++ ui/xadmin/js/checklogin.js
@@ -0,0 +1,24 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkCookie() {
+    var username = getCookie("username");
+    if (username == "") {
+        // alert("Welcome again " + username);
+        window.location.href = "login.html"
+    }
+}
+
+checkCookie()
