4aed12339575f265344053438b889e98d296fad7
rickeyliao <rickey.liao@tutanota.com>
2019-11-02 14:04:21 +0800

    add to repo

---
+++ wifiap/raspi3bplus/change2eth.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+cp $1/fcl.config.eth0 /root/.fcl/fcl.config
+cp $1/rc.local.eth0 /etc/rc.local
+
---
+++ wifiap/raspi3bplus/change2ppp.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+cp $1/fcl.config.ppp0 /root/.fcl/fcl.config
+cp $1/rc.local.ppp0 /etc/rc.local
+
